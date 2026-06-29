fd9e4fa568fd4fca1a32b56e81f6309af66ad02b
Jassiiimm <owainatijassim@gmail.com>
2026-06-29 15:08:45 +0300

    if the box has a color it cannot change its color if clicked again

--- js/app.js
+++ js/app.js
@@ -5,7 +5,10 @@ board.addEventListener("click", function(event) {
     const clickedBox = event.target
 
     if (clickedBox.classList.contains("box")) {
-    
+        if ((clickedBox.classList.contains("red")) || clickedBox.classList.contains("yellow")) {
+            return
+        
+        }
         clickedBox.classList.add(currentPlayer)
 
     } if (currentPlayer === "red") {
