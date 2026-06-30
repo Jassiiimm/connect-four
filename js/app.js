a16bf878f08a144b363159528b14239b8e5d7f2a
Jassiiimm <owainatijassim@gmail.com>
2026-06-30 13:00:29 +0300

    Added the row win

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let currentPlayer = "red"
 const message = document.querySelector("#message")
 let gameOver = false
 
+
   board.addEventListener("click", function(event) {
   const clickedBox = event.target
 
@@ -21,7 +22,17 @@ if (clickedBox.classList.contains("box")) {
          gameOver = true 
          return
         }
+        const rowStart = boxId - (boxId % 7)
+        const rowEnd = rowStart+6
         
+        if ((boxId - 3 >= rowStart && boxes[boxId - 1].classList.contains(currentPlayer) && boxes[boxId - 2].classList.contains(currentPlayer) && boxes[boxId - 3].classList.contains(currentPlayer))
+         || (boxId - 2 >= rowStart && boxId + 1 <= rowEnd && boxes[boxId - 1].classList.contains(currentPlayer) && boxes[boxId - 2].classList.contains(currentPlayer) && boxes[boxId + 1].classList.contains(currentPlayer)) 
+         || (boxId - 1 >= rowStart && boxId + 2 <= rowEnd && boxes[boxId - 1].classList.contains(currentPlayer) && boxes[boxId + 1].classList.contains(currentPlayer) && boxes[boxId + 2].classList.contains(currentPlayer)) 
+         || (boxId + 3 <= rowEnd && boxes[boxId + 1].classList.contains(currentPlayer) && boxes[boxId + 2].classList.contains(currentPlayer) && boxes[boxId + 3].classList.contains(currentPlayer))) {
+          message.textContent = currentPlayer + " wins"
+          gameOver = true
+          return
+        }
         if (currentPlayer === "red") {
           currentPlayer = "yellow"
         } 
