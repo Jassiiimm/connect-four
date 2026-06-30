a59988628675bcabc268e3f5afcb3499f86f1067
Jassiiimm <owainatijassim@gmail.com>
2026-06-30 09:16:51 +0300

    made the pieces drop in columns

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 const board = document.querySelector("#board")
+const boxes = document.querySelectorAll(".box")
+
 let currentPlayer = "red"
 
 board.addEventListener("click", function(event) {
-    const clickedBox = event.target
+  const clickedBox = event.target
 
-    if (clickedBox.classList.contains("box")) {
-        if ((clickedBox.classList.contains("red")) || clickedBox.classList.contains("yellow")) {
-            return
-        
-        }
-        clickedBox.classList.add(currentPlayer)
-
-    } if (currentPlayer === "red") {
-        currentPlayer ="yellow"
-    } else {
-        currentPlayer = "red"
+  if (clickedBox.classList.contains("box")) {
+    const clickedId = Number(clickedBox.id)
+    const column = clickedId % 7
+    console.log(column)
     
-    }
+    for (let boxId = column + 35; boxId >= 0; boxId -= 7) {
+      const box = boxes[boxId]
+      
+      if (!box.classList.contains("red") && !box.classList.contains("yellow")) {
+        box.classList.add(currentPlayer)
+        
+        if (currentPlayer === "red") {
+          currentPlayer = "yellow"
+        } else {
+          currentPlayer = "red"
+        } return
+   }
+  }
+  }
 })
-
