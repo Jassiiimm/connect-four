566076e7a0fa357ec36a5458a29e10d23e46385b
Jassiiimm <owainatijassim@gmail.com>
2026-06-30 10:32:54 +0300

    Added the vertical win

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
 const board = document.querySelector("#board")
 const boxes = document.querySelectorAll(".box")
-
 let currentPlayer = "red"
+const message = document.querySelector("#message")
+let gameOver = false
 
-board.addEventListener("click", function(event) {
+  board.addEventListener("click", function(event) {
   const clickedBox = event.target
 
-  if (clickedBox.classList.contains("box")) {
+if (clickedBox.classList.contains("box")) {
     const clickedId = Number(clickedBox.id)
     const column = clickedId % 7
     console.log(column)
     
     for (let boxId = column + 35; boxId >= 0; boxId -= 7) {
       const box = boxes[boxId]
-      
       if (!box.classList.contains("red") && !box.classList.contains("yellow")) {
         box.classList.add(currentPlayer)
+        if (boxId <=20 && boxes[boxId +7].classList.contains(currentPlayer) && boxes[boxId+14].classList.contains(currentPlayer) && boxes[boxId+21].classList.contains(currentPlayer)) {
+         message.textContent = currentPlayer + " wins" 
+         gameOver = true 
+         return
+        }
         
         if (currentPlayer === "red") {
           currentPlayer = "yellow"
-        } else {
+        } 
+        else {
           currentPlayer = "red"
         } return
-   }
+  }
   }
   }
 })
