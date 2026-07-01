147cbb821c1a9e64b54ec0f4af2354f3256af90c
Jassiiimm <owainatijassim@gmail.com>
2026-07-01 22:01:19 +0300

    Finalize project

--- js/app.js
+++ js/app.js
@@ -14,17 +14,17 @@ let playerOneWins = 0
 let playerTwoWins = 0
 
 
-  board.addEventListener("click", function(event) {
+board.addEventListener("click", function (event) {
   const clickedBox = event.target
 
-if (clickedBox.classList.contains("box")) {
-  if (gameStarted === false) {
-    message.textContent = "Click start game first"
-    return
-  }
-  if (gameOver === true) {
-    return
-  }
+  if (clickedBox.classList.contains("box")) {
+    if (gameStarted === false) {
+      message.textContent = "Click start game first"
+      return
+    }
+    if (gameOver === true) {
+      return
+    }
 
     const clickedId = Number(clickedBox.id)
     const column = clickedId % 7
@@ -35,226 +35,226 @@ if (clickedBox.classList.contains("box")) {
       if (!box.classList.contains("red") && !box.classList.contains("yellow")) {
         box.classList.add(currentPlayer)
 
-        if (boxId <=20 && boxes[boxId +7].classList.contains(currentPlayer)
-          && boxes[boxId+14].classList.contains(currentPlayer)
-        && boxes[boxId+21].classList.contains(currentPlayer)) {
-         message.textContent = currentPlayer + " wins"
-         gameOver = true
-
-         if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
-        }
-
-        return
-      }
-
-      const rowStart = boxId - (boxId % 7)
-      const rowEnd = rowStart+6
-
-        if ((boxId - 3 >= rowStart
-          && boxes[boxId - 1].classList.contains(currentPlayer)
-            && boxes[boxId - 2].classList.contains(currentPlayer)
-           && boxes[boxId - 3].classList.contains(currentPlayer))
-         || (boxId - 2 >= rowStart
-          && boxId + 1 <= rowEnd
-            && boxes[boxId - 1].classList.contains(currentPlayer)
-          && boxes[boxId - 2].classList.contains(currentPlayer)
-           && boxes[boxId + 1].classList.contains(currentPlayer))
-         || (boxId - 1 >= rowStart
-           && boxId + 2 <= rowEnd
-             && boxes[boxId - 1].classList.contains(currentPlayer)
-           && boxes[boxId + 1].classList.contains(currentPlayer)
-            && boxes[boxId + 2].classList.contains(currentPlayer))
-         ||  (boxId + 3 <= rowEnd
-          && boxes[boxId + 1].classList.contains(currentPlayer)
-          && boxes[boxId + 2].classList.contains(currentPlayer)
-          && boxes[boxId + 3].classList.contains(currentPlayer))) {
+        if (boxId <= 20 && boxes[boxId + 7].classList.contains(currentPlayer)
+          && boxes[boxId + 14].classList.contains(currentPlayer)
+          && boxes[boxId + 21].classList.contains(currentPlayer)) {
           message.textContent = currentPlayer + " wins"
           gameOver = true
+
           if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
-        }
+            playerOneWins++
+            playerOneScore.textContent = playerOneWins
+          } else {
+            playerTwoWins++
+            playerTwoScore.textContent = playerTwoWins
+          }
+
           return
         }
 
-     const row = Math.floor(boxId / 7)
-const columnNumber = boxId % 7
+        const rowStart = boxId - (boxId % 7)
+        const rowEnd = rowStart + 6
 
-// \ diagonal (row - is up, column - left)
-if (row - 3 >= 0 && columnNumber - 3 >= 0) {
-  if ( boxes[boxId - 8].classList.contains(currentPlayer)
-    && boxes[boxId - 16].classList.contains(currentPlayer)
-  && boxes[boxId - 24].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if ((boxId - 3 >= rowStart
+          && boxes[boxId - 1].classList.contains(currentPlayer)
+          && boxes[boxId - 2].classList.contains(currentPlayer)
+          && boxes[boxId - 3].classList.contains(currentPlayer))
+          || (boxId - 2 >= rowStart
+            && boxId + 1 <= rowEnd
+            && boxes[boxId - 1].classList.contains(currentPlayer)
+            && boxes[boxId - 2].classList.contains(currentPlayer)
+            && boxes[boxId + 1].classList.contains(currentPlayer))
+          || (boxId - 1 >= rowStart
+            && boxId + 2 <= rowEnd
+            && boxes[boxId - 1].classList.contains(currentPlayer)
+            && boxes[boxId + 1].classList.contains(currentPlayer)
+            && boxes[boxId + 2].classList.contains(currentPlayer))
+          || (boxId + 3 <= rowEnd
+            && boxes[boxId + 1].classList.contains(currentPlayer)
+            && boxes[boxId + 2].classList.contains(currentPlayer)
+            && boxes[boxId + 3].classList.contains(currentPlayer))) {
+          message.textContent = currentPlayer + " wins"
+          gameOver = true
+          if (currentPlayer === "red") {
+            playerOneWins++
+            playerOneScore.textContent = playerOneWins
+          } else {
+            playerTwoWins++
+            playerTwoScore.textContent = playerTwoWins
+          }
+          return
         }
-    return
-  }
-}
 
-if (row + 3 <= 5 && columnNumber + 3 <= 6) {
-  if (boxes[boxId + 8].classList.contains(currentPlayer)
-    && boxes[boxId + 16].classList.contains(currentPlayer)
-  && boxes[boxId + 24].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        const row = Math.floor(boxId / 7)
+        const columnNumber = boxId % 7
+
+        // \ diagonal (row - is up, column - left)
+        if (row - 3 >= 0 && columnNumber - 3 >= 0) {
+          if (boxes[boxId - 8].classList.contains(currentPlayer)
+            && boxes[boxId - 16].classList.contains(currentPlayer)
+            && boxes[boxId - 24].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-if (row - 2 >= 0 && columnNumber - 2 >= 0 && row + 1 <= 5 && columnNumber + 1 <= 6) {
-  if (boxes[boxId - 8].classList.contains(currentPlayer)
-    && boxes[boxId - 16].classList.contains(currentPlayer)
-  && boxes[boxId + 8].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if (row + 3 <= 5 && columnNumber + 3 <= 6) {
+          if (boxes[boxId + 8].classList.contains(currentPlayer)
+            && boxes[boxId + 16].classList.contains(currentPlayer)
+            && boxes[boxId + 24].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-if (row - 1 >= 0 && columnNumber - 1 >= 0 && row + 2 <= 5 && columnNumber + 2 <= 6) {
-  if (boxes[boxId - 8].classList.contains(currentPlayer)
-    && boxes[boxId + 8].classList.contains(currentPlayer)
-  && boxes[boxId + 16].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if (row - 2 >= 0 && columnNumber - 2 >= 0 && row + 1 <= 5 && columnNumber + 1 <= 6) {
+          if (boxes[boxId - 8].classList.contains(currentPlayer)
+            && boxes[boxId - 16].classList.contains(currentPlayer)
+            && boxes[boxId + 8].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-// / diagonal (same idea just flip the signs and change 8 to 6)
-
-if (row - 3 >= 0 && columnNumber + 3 <= 6) {
-  if (boxes[boxId - 6].classList.contains(currentPlayer)
-    && boxes[boxId - 12].classList.contains(currentPlayer)
-  && boxes[boxId - 18].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if (row - 1 >= 0 && columnNumber - 1 >= 0 && row + 2 <= 5 && columnNumber + 2 <= 6) {
+          if (boxes[boxId - 8].classList.contains(currentPlayer)
+            && boxes[boxId + 8].classList.contains(currentPlayer)
+            && boxes[boxId + 16].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-if (row + 3 <= 5 && columnNumber - 3 >= 0) {
-  if (boxes[boxId + 6].classList.contains(currentPlayer)
-    && boxes[boxId + 12].classList.contains(currentPlayer)
-  && boxes[boxId + 18].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        // / diagonal (same idea just flip the signs and change 8 to 6)
+
+        if (row - 3 >= 0 && columnNumber + 3 <= 6) {
+          if (boxes[boxId - 6].classList.contains(currentPlayer)
+            && boxes[boxId - 12].classList.contains(currentPlayer)
+            && boxes[boxId - 18].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-if (row - 2 >= 0 && columnNumber + 2 <= 6 && row + 1 <= 5 && columnNumber - 1 >= 0) {
-  if (boxes[boxId - 6].classList.contains(currentPlayer)
-    && boxes[boxId - 12].classList.contains(currentPlayer)
-  && boxes[boxId + 6].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if (row + 3 <= 5 && columnNumber - 3 >= 0) {
+          if (boxes[boxId + 6].classList.contains(currentPlayer)
+            && boxes[boxId + 12].classList.contains(currentPlayer)
+            && boxes[boxId + 18].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-if (row - 1 >= 0 && columnNumber + 1 <= 6 && row + 2 <= 5 && columnNumber - 2 >= 0) {
-  if (boxes[boxId - 6].classList.contains(currentPlayer)
-    && boxes[boxId + 6].classList.contains(currentPlayer)
-    && boxes[boxId + 12].classList.contains(currentPlayer)) {
-    message.textContent = currentPlayer + " wins"
-    gameOver = true
-    if (currentPlayer === "red") {
-          playerOneWins++
-          playerOneScore.textContent = playerOneWins
-        } else {
-          playerTwoWins++
-          playerTwoScore.textContent = playerTwoWins
+        if (row - 2 >= 0 && columnNumber + 2 <= 6 && row + 1 <= 5 && columnNumber - 1 >= 0) {
+          if (boxes[boxId - 6].classList.contains(currentPlayer)
+            && boxes[boxId - 12].classList.contains(currentPlayer)
+            && boxes[boxId + 6].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
         }
-    return
-  }
-}
 
-let tie = true
+        if (row - 1 >= 0 && columnNumber + 1 <= 6 && row + 2 <= 5 && columnNumber - 2 >= 0) {
+          if (boxes[boxId - 6].classList.contains(currentPlayer)
+            && boxes[boxId + 6].classList.contains(currentPlayer)
+            && boxes[boxId + 12].classList.contains(currentPlayer)) {
+            message.textContent = currentPlayer + " wins"
+            gameOver = true
+            if (currentPlayer === "red") {
+              playerOneWins++
+              playerOneScore.textContent = playerOneWins
+            } else {
+              playerTwoWins++
+              playerTwoScore.textContent = playerTwoWins
+            }
+            return
+          }
+        }
 
-for (let i = 0; i < boxes.length; i++) {
-  if (!boxes[i].classList.contains("red") && !boxes[i].classList.contains("yellow")) {
-    tie = false
-  }
-}
+        let tie = true
 
-if (tie === true) {
-  message.textContent = "It's a tie!!!"
-  gameOver = true
-  return
-}
+        for (let i = 0; i < boxes.length; i++) {
+          if (!boxes[i].classList.contains("red") && !boxes[i].classList.contains("yellow")) {
+            tie = false
+          }
+        }
 
-if (currentPlayer === "red") {
+        if (tie === true) {
+          message.textContent = "It's a tie!!!"
+          gameOver = true
+          return
+        }
+
+        if (currentPlayer === "red") {
           currentPlayer = "yellow"
         }
         else {
           currentPlayer = "red"
         }
-      message.textContent = currentPlayer + "'s turn"
+        message.textContent = currentPlayer + "'s turn"
         return
-  }
-  }
+      }
+    }
   }
 })
 
-playAgainBtn.addEventListener("click", function() {
+playAgainBtn.addEventListener("click", function () {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].classList.remove("red")
     boxes[i].classList.remove("yellow")
@@ -266,7 +266,7 @@ playAgainBtn.addEventListener("click", function() {
   message.textContent = "red's turn"
 })
 
-resetBtn.addEventListener("click", function() {
+resetBtn.addEventListener("click", function () {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].classList.remove("red")
     boxes[i].classList.remove("yellow")
@@ -286,7 +286,7 @@ resetBtn.addEventListener("click", function() {
   startBtn.classList.remove("hide")
 })
 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function () {
   gameStarted = true
   gameOver = false
   currentPlayer = "red"
